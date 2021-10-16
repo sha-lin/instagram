d7543c53bc7fdc7e48f59bb28aadb8d31c6e4210
shalin <shalin.rono@student.moringaschool.com>
2021-10-16 20:37:12 +0300

    create a welcome email for users after signing up

--- gram/views.py
+++ gram/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect, get_object_or_404
+from django.http import Http404,HttpResponseRedirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from . forms import Registration,UpdateUser,UpdateProfile,CommentsForm,postPhotoForm
+from django.contrib.auth.decorators import login_required
+from .models import Image,Profile,Like,Follows
+from django.http import JsonResponse
+from django.contrib.auth.models import User
+from .email import send_welcome_email
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
