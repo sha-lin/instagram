9ad3e926236a6a7d848cbbb2be5a0f92d3b493dc
shalin <shalin.rono@student.moringaschool.com>
2021-10-16 20:41:52 +0300

    create welcome text via email

--- gram/email.py
+++ gram/email.py
@@ -2,8 +2,8 @@ from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
 
 def send_welcome_email(name,receiver):
-  subject = 'Welcome'
-  sender = 'ngetij.nick.com'
+  subject = 'Welcome to your instagram account'
+  sender = 'shalin.rono@student.moringaschool.com'
   text_content = render_to_string('email/welcome.txt',{"name":name})
   html_content = render_to_string('email/welcome.html',{"name":name})
   msg = EmailMultiAlternatives(subject,text_content,sender,[receiver])
