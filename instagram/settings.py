1e2fd0a426a46f56c240f7e6d39c93021682d2d2
shalin <shalin.rono@student.moringaschool.com>
2021-10-17 16:58:52 +0300

    media settings changes

--- instagram/settings.py
+++ instagram/settings.py
@@ -134,8 +134,10 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 
 # adding config
